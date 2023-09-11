0313307e31d49676ae765cf0a4ad4614174de518
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-11 21:42:36 +0500

    'practice2'

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,12 @@ const Card = (props) => {
     } = props
     console.log(apiData);
   return (
-    <div className='card' style={{display: 'flex' , flexDirection:'column'}}>
+    <div className='card'>
       {
         apiData.map((data) => {
             console.log(data.title);
-           return <div key={data.id} width={400} className='App-logo'>
-                <img src={data.image} alt="" width={300} height={300} />
+           return <div key={data.id} width={400} style={{border : '1px solid #61dafb' , padding: '20px' , margin: '20px 20px' , borderRadius : '5px'}}>
+                <img src={data.image} alt="" className='App-logo' width={250} height={300} style={{borderRadius : '50%'}} />
                 <h2 style={{fontWeight : 'bold'}}>{data.title}</h2>
                 <p style={{fontWeight : '600'}}>{data.desc}</p>
             </div>
